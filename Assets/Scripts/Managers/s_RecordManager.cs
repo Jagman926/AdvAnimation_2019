c36467451bfe88287837ae4d9bbf85249b7b4583
Josh_Main <joshuagrazda@gmail.com>
2019-09-20 21:35:05 -0400

    Merged Record Script into Manager
    Changed:
    - Removed s_Record for Record tag
    - Merged code into Record Manager

--- Assets/Scripts/Managers/s_RecordManager.cs
+++ Assets/Scripts/Managers/s_RecordManager.cs
@@ -13,40 +13,119 @@ namespace Managers
             FORWARD
         }
 
+        [SerializeField]
+        public struct TransformRecording
+        {
+            public List<Vector3> positionsList;
+            public List<Quaternion> rotationsList;
+        }
+
+        [Header("Object Recordings")]
+        [SerializeField]
+        private GameObject[] objectToRecord;
+        public List<TransformRecording> transformsList;
+
         [Header("Record Variables")]
         public bool record;
-        public float recordStep;
+        public float keyframeStep;
+        public float keyframeTime;
         public int currentFrame;
         public float recordDuration;
         public float currentPlaybackTime;
 
         [Header("Playback Variables")]
         public bool playback;
+        private bool inPlayback;
 
         void Start()
         {
+            // Init starting variables
             currentFrame = 0;
             currentPlaybackTime = 0.0f;
+            keyframeTime = 0.0f;
+            // Init Objects to record
+            GetObjectsToRecord();
         }
 
         void FixedUpdate()
         {
-            if(record)
+            if (record)
+            {
+                // Update keyframe time
+                keyframeTime += Time.fixedDeltaTime;
+            }
+            if (record && keyframeTime > keyframeStep)
             {
                 recordDuration = Time.fixedTime;
+                RecordFrames();
             }
-            if(playback)
+            else if (!record && playback)
             {
                 currentPlaybackTime += Time.fixedDeltaTime;
-                if(currentPlaybackTime > recordDuration)
+                if (currentPlaybackTime > recordDuration)
                     currentPlaybackTime = 0.0f;
                 UpdateCurrentFrame();
+                PlaybackFrames();
+            }
+            else
+            {
+                foreach (GameObject obj in objectToRecord)
+                {
+                    obj.GetComponent<Rigidbody>().isKinematic = false;
+                }
+            }
+        }
+
+        void GetObjectsToRecord()
+        {
+            objectToRecord = GameObject.FindGameObjectsWithTag("Record");
+            // Init List
+            transformsList = new List<TransformRecording>();
+            for (int i = 0; i < objectToRecord.Length; i++)
+            {
+                TransformRecording tempRecord;
+                tempRecord.positionsList = new List<Vector3>();
+                tempRecord.rotationsList = new List<Quaternion>();
+                transformsList.Add(tempRecord);
+            }
+        }
+
+        void RecordFrames()
+        {
+            for (int i = 0; i < objectToRecord.Length; i++)
+            {
+                transformsList[i].positionsList.Add(objectToRecord[i].transform.position);
+                transformsList[i].rotationsList.Add(objectToRecord[i].transform.rotation);
+            }
+            // Reset keyframeTime
+            keyframeTime = 0.0f;
+        }
+
+        void PlaybackFrames()
+        {
+            if (!inPlayback)
+            {
+                foreach (GameObject obj in objectToRecord)
+                {
+                    obj.GetComponent<Rigidbody>().isKinematic = false;
+                }
+                inPlayback = true;
+            }
+
+            if (currentFrame < transformsList[0].positionsList.Count)
+            {
+                for (int i = 0; i < objectToRecord.Length; i++)
+                {
+                    // Set position and rotation to current frame
+                    objectToRecord[i].transform.position = transformsList[i].positionsList[currentFrame];
+                    objectToRecord[i].transform.rotation = transformsList[i].rotationsList[currentFrame];
+                }
             }
         }
 
         void UpdateCurrentFrame()
         {
-            currentFrame = Mathf.RoundToInt(currentPlaybackTime / recordStep);
+            currentFrame = Mathf.RoundToInt(currentPlaybackTime / keyframeStep);
         }
     }
 }
