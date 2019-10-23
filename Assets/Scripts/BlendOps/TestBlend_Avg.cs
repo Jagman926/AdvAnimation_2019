e104f3f83ae39a36cda1d62d138256cf0ba294f8
Josh_Laptop <joshuagrazda@yahoo.com>
2019-10-23 15:04:18 -0400

    Scene adjustment and script work
    Changed:
    - Added scripts to corresponding assets in scene
    - Implemented Scale
    - Started Average

--- Assets/Scripts/BlendOps/TestBlend_Avg.cs
+++ Assets/Scripts/BlendOps/TestBlend_Avg.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class TestBlend_Avg : TestBlend
 {
+    [SerializeField]
+    private Transform poseIdentity;
+    [SerializeField]
+    private List<Transform> posesList;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private List<float> weightsList;
+
     void Start()
     {
-        
+        // Makes sure there is a weight for every pose and vice versa
+        VerifyLists();
     }
 
     void Update()
     {
-        
+        // For each pose added into pose list, lerp between identity and current pose by weight (acts as a parameter) and then save the resulting pose
+        // and continue for the next pose in the list until the list is completed and all poses have been averaged in
+    }
+
+    private void VerifyLists()
+    {
+        if (posesList.Count > weightsList.Count)
+            Debug.LogWarning("You are missing weights for your poses in TestBlend_Avg");
+        else if (posesList.Count < weightsList.Count)
+            Debug.LogWarning("You are missing poses for your weights in TestBlend_Avg");
     }
 }
--- Assets/Scripts/BlendOps/TestBlend_Scale.cs
+++ Assets/Scripts/BlendOps/TestBlend_Scale.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class TestBlend_Scale : TestBlend
 {
     [SerializeField]
+    private Transform poseIdentity = null;
+    [SerializeField]
     private Transform pose1 = null;
 
     [Range(0.0f, 1.0f)]
-    public float paramater = 1.0f;
+    public float parameter = 0.0f;
 
     void Update()
     {
-        // LOGIC: same as lerp operation as if pose0 is poseIdentity
+        // Translation: lteral linear interpolation
+        pose_result.localPosition = Vector3.Lerp(poseIdentity.localPosition, pose1.localPosition, parameter);
+
+        // scale: ditto
+        pose_result.localScale = Vector3.Lerp(poseIdentity.localScale, pose1.localScale, parameter);
+
+        // rotation: quaternion SLERP or Euler LERP
+        if (usingQuaternionRotation)
+            pose_result.localRotation = Quaternion.Slerp(poseIdentity.localRotation, pose1.localRotation, parameter);
+        else
+            pose_result.localEulerAngles = Vector3.Lerp(poseIdentity.localEulerAngles, pose1.localEulerAngles, parameter);
     }
 }
