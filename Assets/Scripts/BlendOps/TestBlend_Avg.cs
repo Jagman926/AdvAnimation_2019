19e0a308268420daa93fe1308ac0a899e5186219
Josh_J101_08 <joshuagrazda@gmail.com>
2019-10-25 12:08:45 -0400

    Working on Blend_Avg
    Changed:
    - implemented proper implementation minus the fact that you can't init new transform in Unity without a gameobject and that is currently making things not work

--- Assets/Scripts/BlendOps/TestBlend_Avg.cs
+++ Assets/Scripts/BlendOps/TestBlend_Avg.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class TestBlend_Avg : TestBlend
 {
     [SerializeField]
-    private Transform poseIdentity;
+    private Transform poseIdentity = null;
     [SerializeField]
-    private List<Transform> posesList;
+    private List<Transform> posesList = null;
     [SerializeField]
     [Range(0.0f, 1.0f)]
-    private List<float> weightsList;
+    private List<float> weightsList = null;
+
+    // Gameobject temps for transforms
+    private List<GameObject> emptyGOs;
+    private List<Transform> poseTransforms;
 
     void Start()
     {
         // Makes sure there is a weight for every pose and vice versa
         VerifyLists();
+        // init transforms list
+        emptyGOs = new List<GameObject>();
+        poseTransforms = new List<Transform>();
+        for (int i = 0; i < posesList.Count; i++)
+        {
+            GameObject tempGO = new GameObject();
+            emptyGOs.Add(tempGO);
+            poseTransforms.Add(emptyGOs[i].transform);
+        }
+        pose_result = poseIdentity;
     }
 
     void Update()
     {
+        VerifyLists();
         // For each pose added into pose list, lerp between identity and current pose by weight (acts as a parameter) and then save the resulting pose
         // and continue for the next pose in the list until the list is completed and all poses have been averaged in
+        for (int i = 0; i < posesList.Count; i++)
+            poseTransforms[i] = Scale(poseIdentity, posesList[i], weightsList[i]);
+
+        for (int j = 0; j < posesList.Count; j++)
+            pose_result = Add(pose_result, poseTransforms[j]);
     }
 
     private void VerifyLists()
@@ -30,5 +50,44 @@ public class TestBlend_Avg : TestBlend
             Debug.LogWarning("You are missing weights for your poses in TestBlend_Avg");
         else if (posesList.Count < weightsList.Count)
             Debug.LogWarning("You are missing poses for your weights in TestBlend_Avg");
+    }
+
+    private Transform Scale(Transform poseIdentity, Transform pose1, float parameter)
+    {
+        GameObject emptyGO = new GameObject();
+        Transform poseTransform = emptyGO.transform;
+
+        // Translation: lteral linear interpolation
+        poseTransform.localPosition = Vector3.Lerp(poseIdentity.localPosition, pose1.localPosition, parameter);
+
+        // scale: ditto
+        poseTransform.localScale = Vector3.Lerp(poseIdentity.localScale, pose1.localScale, parameter);
+
+        // rotation: quaternion SLERP or Euler LERP
+        if (usingQuaternionRotation)
+            poseTransform.localRotation = Quaternion.Slerp(poseIdentity.localRotation, pose1.localRotation, parameter);
+        else
+            poseTransform.localEulerAngles = Vector3.Lerp(poseIdentity.localEulerAngles, pose1.localEulerAngles, parameter);
+
+        return poseTransform;
+    }
+
+    private Transform Add(Transform pose0, Transform pose1)
+    {
+        GameObject emptyGO = new GameObject();
+        Transform poseTransform = emptyGO.transform;
+
+        // add transforms
+        poseTransform.position = pose0.position + pose1.position;
+        // multiplication of scales
+        poseTransform.localScale = pose0.localScale;
+        poseTransform.localScale.Scale(pose1.localScale);
+        // multiply quaternions
+        if (usingQuaternionRotation)
+            poseTransform.localRotation = pose0.localRotation * pose1.localRotation;
+        else
+            poseTransform.localEulerAngles = pose0.localEulerAngles + pose1.localEulerAngles;
+
+        return poseTransform;
     }
 }
